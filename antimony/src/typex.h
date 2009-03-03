a87cecc4bf13e5954aee0b4a44f80766b8c2c1a0
lpsmith <lpsmith@17825c85-ebf9-2b49-9e67-686c1b10f5d8>
2009-03-03 00:22:07 +0000

    Whoops, forgot the header file.

---
+++ antimony/src/typex.h
@@ -0,0 +1,16 @@
+#ifndef ANTIMONY_TYPEX_H
+#define ANTIMONY_TYPEX_H
+
+#include "enums.h"
+#include <string>
+bool IsReaction(const var_type vtype);
+bool IsInteraction(const rd_type rdtype);
+bool IsSpecies(const var_type vtype);
+bool IsDNA(const var_type vtype);
+bool HasOrIsFormula(const var_type vtype);
+std::string RDToString(rd_type type);
+std::string VarTypeToString(const var_type vtype);
+std::string VarTypeToAntimony(const var_type vtype);
+std::string ReturnTypeToString(return_type rtype);
+
+#endif
