00b6e727184ef2b20b13ca8e8fe5a4e07a7e72cd
Frank Bergmann <949059+fbergmann@users.noreply.github.com>
2009-07-21 03:31:36 +0000

    - SBW integration complete ... now have registration working too ...

--- QTAntimony/main.cpp
+++ QTAntimony/main.cpp
@@ -3,38 +3,8 @@
 #include "QTAntimony.h"
 
 #ifdef SBW_INTEGRATION
-
-#include "SBW/SBW.h"
-#include "QEvent"
-#include <string>
-using namespace std;
-using namespace SystemsBiologyWorkbench;
-
-
-// QT Antimony SBW Service ... just load an SBML file, send it to the application .. thats it
-class SBWAntimony : SystemsBiologyWorkbench::SBWListener
-{
-public:
-	SBWAntimony() {}
-
-	virtual void onShutdown() { exit(0); }
-
-		static void registerMethods(MethodTable<SBWAntimony> &table)
-		{
-			table.addMethod(
-				&SBWAntimony::doAnalysisImpl,"void doAnalysis(string)",0,""
-				);
-		}
-		DataBlockWriter doAnalysisImpl(Module /*from*/, DataBlockReader arguments)
-		{
-			string sbml; arguments >> sbml;
-			qApp->postEvent(qApp, new QSBMLEvent(sbml));
-			return DataBlockWriter();
-		}
-};
-
+#include "SBW/ModuleImpl.h"
 #endif
-
 
 void myMessageOutput(QtMsgType type, const char *msg)
  {
@@ -60,8 +30,46 @@ void myMessageOutput(QtMsgType type, const char *msg)
 
 int main(int argc, char *argv[])
 {
+	
+#ifdef SBW_INTEGRATION
+	
+	SBWAntimony *service = new SBWAntimony();
+	
+	static const std::string Name("QTAntimony");
+	static const std::string ServiceName("QTAntimony");
+	static const std::string DisplayName("QTAntimony 1.0");
+	static const std::string HelpString("Antimony analyzer");
+	static const std::string CategoryName("/Analysis");
+		
+	ModuleImpl oModule(Name, DisplayName, UniqueModule, HelpString);
+	oModule.addServiceObject(ServiceName, DisplayName, CategoryName, service, HelpString);
+
+	// register if neccessary
+    for (int arg=1; arg<argc; arg++) {
+        string sArg(argv[arg]);
+		if (sArg == "-sbwregister" || sArg == "--sbwregister")
+		{
+			oModule.run(argc, argv);
+			return 0;
+		}
+    }
+	
+	// otherwise enable SBW service
+	oModule.enableModuleServices();
+	
+	
+#endif	
+	
     qInstallMsgHandler(myMessageOutput);
     QTAntimony a(argc, argv);
+
+#ifdef SBW_INTEGRATION
+
+	// install an event filter that will display a new window  when SBML is loaded
+    a.installEventFilter(&a);
+
+#endif
+
     for (int arg=1; arg<argc; arg++) {
         a.OpenFile(argv[arg]);
     }
@@ -72,26 +80,6 @@ int main(int argc, char *argv[])
         newwindow->show();
         */
     }
-
-#ifdef SBW_INTEGRATION
-
-	try
-	{
-		SBW::disconnect();
-		ModuleImpl oModule("QTAntimony", "QTAntimony", UniqueModule, "Antimony analyzer");
-		SBWAntimony service;
-		oModule.addServiceObject("analysis", "QT Antimony", "/Analysis", &service, "Antimony analyzer");
-		oModule.enableModuleServices();
-
-		// install an event filter that will display a new window  when SBML is loaded
-		a.installEventFilter(&a);
-	}
-	catch (...)
-	{
-		// oups
-	}
-
-#endif
 
     return a.exec();
 }
