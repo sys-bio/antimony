be40cec673bbf79ccb03e91695d86ed8e69220d7
Lucian Smith <1736150+luciansmith@users.noreply.github.com>
2009-08-21 20:15:04 +0000

    -Fixed leak in testantimony and another in module; -Revamped 'new' and 'delete' syntax to be consistent -Fixed memory bug with 'setlocale' -Commented out FileWatcher functionality since I wasn't using it for anything and it was issuing error messages right and left.

--- QTAntimony/FileWatcher.cpp
+++ QTAntimony/FileWatcher.cpp
@@ -1,11 +1,12 @@
 #include "FileWatcher.h"
 
+//LS DEBUG:  This wasn't doing anything but issuing warnings, so they're commented out for now.
 void FileWatcher::StartWatching(const QString& filename)
 {
-    addPath(filename);
+    //addPath(filename);
 }
 
 void FileWatcher::StopWatching(const QString& filename)
 {
-    removePath(filename);
+    //removePath(filename);
 }
