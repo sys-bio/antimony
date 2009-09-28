d76faefa9cc6d9e0111058de96422754b01fa04f
Lucian Smith <1736150+luciansmith@users.noreply.github.com>
2009-09-28 22:45:20 +0000

    Getting rid of warnings.

--- src/Antimony.cpp
+++ src/Antimony.cpp
@@ -6,9 +6,9 @@
 #ifdef WIN32
 #include <windows.h>
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
+BOOL APIENTRY DllMain( HMODULE /*hModule*/,
+                       DWORD  /*ul_reason_for_call*/,
+                       LPVOID /*lpReserved*/
                                          )
 {
     return TRUE;
