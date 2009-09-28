989fa4bfdbafd8dc260f7f2585c06d5908239d87
Lucian Smith <1736150+luciansmith@users.noreply.github.com>
2009-09-28 21:52:51 +0000

    Fixing issues with SBW integration.

--- QTAntimony/main.cpp
+++ QTAntimony/main.cpp
@@ -8,82 +8,86 @@
 #endif
 
 void myMessageOutput(QtMsgType type, const char *msg)
- {
-     switch (type) {
-     case QtDebugMsg:
+{
+	switch (type) {
+	 case QtDebugMsg:
 #ifndef NDEBUG
-         fprintf(stderr, "Debug: %s\n", msg);
+		 fprintf(stderr, "Debug: %s\n", msg);
 #endif
-         break;
-     case QtWarningMsg:
+		 break;
+	 case QtWarningMsg:
 #ifndef NDEBUG
-         fprintf(stderr, "Warning: %s\n", msg);
+		 fprintf(stderr, "Warning: %s\n", msg);
 #endif
-         break;
-     case QtCriticalMsg:
-         fprintf(stderr, "Critical error from QT: %s\n", msg);
-         break;
-     case QtFatalMsg:
-         fprintf(stderr, "Fatal error from QT: %s\n", msg);
-         abort();
-     }
- }
+		 break;
+	 case QtCriticalMsg:
+		 fprintf(stderr, "Critical error from QT: %s\n", msg);
+		 break;
+	 case QtFatalMsg:
+		 fprintf(stderr, "Fatal error from QT: %s\n", msg);
+		 abort();
+	}
+}
 
 int main(int argc, char *argv[])
 {
-	
+
 #ifdef SBW_INTEGRATION
-    bool sbwon = true;
-    try {
+	bool sbwon = true;
 	SBWAntimony service;
-	
-	static const std::string Name("QTAntimony");
-	static const std::string ServiceName("QTAntimony");
-	static const std::string DisplayName("QTAntimony 1.0");
-	static const std::string HelpString("Antimony analyzer");
-	static const std::string CategoryName("/Analysis");
-		
+
+	static const char* Name("QTAntimony");
+	static const char* ServiceName("QTAntimony");
+	static const char* DisplayName("QTAntimony 1.0");
+	static const char* HelpString("Antimony analyzer");
+	static const char* CategoryName("/Analysis");
+
 	ModuleImpl oModule(Name, DisplayName, UniqueModule, HelpString);
 	oModule.addServiceObject(ServiceName, DisplayName, CategoryName, &service, HelpString);
-
-	// register if neccessary
-        for (int arg=1; arg<argc; arg++) {
-            std::string sArg(argv[arg]);
-                if (sArg == "-sbwregister" || sArg == "--sbwregister")
-                {
-                    oModule.run(argc, argv);
-                    return 0;
+	
+	int sbwModuleIndex = -1;
+	try {
+		// register if neccessary
+		for (int arg=1; arg<argc; arg++) 
+		{
+			std::string sArg(argv[arg]);
+			if (sArg == "-sbwregister" || sArg == "--sbwregister")
+			{
+				oModule.run(argc, argv);
+				return 0;
+			}
+			else if (sArg == "-sbwregister" || sArg == "--sbwregister")
+				sbwModuleIndex = arg;
 		}
-        }
-	// otherwise enable SBW service
-	oModule.enableModuleServices();
-    }
-    catch (...) {
-        sbwon = false;
-    }
-	
+		// otherwise enable SBW service
+		oModule.enableModuleServices();
+	}
+	catch (...) {
+		sbwon = false;
+	}
+
 #endif
-	
-    qInstallMsgHandler(myMessageOutput);
-    QTAntimony a(argc, argv);
+
+	qInstallMsgHandler(myMessageOutput);
+	QTAntimony a(argc, argv);
 
 #ifdef SBW_INTEGRATION
 
-    // install an event filter that will display a new window when SBML is loaded
-    if (sbwon) {
-        a.installEventFilter(&a);
-    }
-    else {
-        a.SetUseSBW(false);
-    }
+	// install an event filter that will display a new window when SBML is loaded
+	if (sbwon) {
+		a.installEventFilter(&a);
+	}
+	else {
+		a.SetUseSBW(false);
+	}
 #endif
 
-    for (int arg=1; arg<argc; arg++) {
-        a.OpenFile(argv[arg]);
-    }
-    if (argc==1) {
-        a.NewWindow();
-    }
+	for (int arg=1; arg<argc; arg++) {
+		if (sbwModuleIndex != arg) a.OpenFile(argv[arg]);
+	}
+	if (argc==1) {
+		a.NewWindow();
+	}
 
-    return a.exec();
+	return a.exec();
 }
