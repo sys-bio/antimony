47b22d5a9d0aa29da8a6bbc4bbd9b8d4d96829dc
Lucian Smith <1736150+luciansmith@users.noreply.github.com>
2009-10-15 19:39:29 +0000

    Argh date wrong in QTA tutorial.

--- QTAntimony/Tutorial.cpp
+++ QTAntimony/Tutorial.cpp
@@ -5,8 +5,9 @@ Tutorial::Tutorial(QWidget* parent)
 {
     setOpenLinks(false);
 #include "Tutorial.xxd"
-    // created using 'xxd -i Tutorial.html' then changing the main bit
-    // to 'static const char' and the _len to 'static const unsigned int'
+    // created using 'xxd -i Tutorial.html > Tutorial.xxd' then changing the
+    // main bit to 'static const char' and the _len to
+    // 'static const unsigned int'
     std::string tutorial_string;
     tutorial_string.assign(Tutorial_html, Tutorial_html_len);
     setHtml(tutorial_string.c_str());
