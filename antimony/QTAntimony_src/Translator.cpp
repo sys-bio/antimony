aab0e5054a5aad249d89703dea1ffcfced3a5cb2
lpsmith <lpsmith@17825c85-ebf9-2b49-9e67-686c1b10f5d8>
2011-07-13 18:58:23 +0000

    New version number.

--- antimony/QTAntimony_src/Translator.cpp
+++ antimony/QTAntimony_src/Translator.cpp
@@ -523,7 +523,7 @@ void Translator::closeEvent(QCloseEvent* event)
 void Translator::DisplayAbout()
 {
    QMessageBox msgBox;
-    msgBox.setText("QTAntimony v0.6.1:");
+    msgBox.setText("QTAntimony " + LIBANTIMONY_VERSION_STRING);
     msgBox.setInformativeText("Based on libAntimony " LIBANTIMONY_VERSION_STRING " and libSBML v" LIBSBML_DOTTED_VERSION);
     msgBox.setStandardButtons(QMessageBox::Ok);
     msgBox.exec();
