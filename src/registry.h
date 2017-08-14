407b687ec6fc1e02488ac715ebd941f504b57fc8
Kyle Medley <0u812@users.noreply.github.com>
2017-08-14 20:07:13 +0000

    bump ver to 2.9.4

--- src/registry.h
+++ src/registry.h
@@ -25,7 +25,7 @@
 
 #define MAINMODULE "__main"
 #ifndef LIBANTIMONY_VERSION_STRING //Should be created in the makefile (CMakeLists.txt)
-#define LIBANTIMONY_VERSION_STRING "v2.9.3"
+#define LIBANTIMONY_VERSION_STRING "v2.9.4"
 #endif
 
 class Registry
