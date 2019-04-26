7e547141095191937ca7f8fdc7bafaa4df1dbc2e
Lucian Smith <1736150+luciansmith@users.noreply.github.com>
2019-04-26 23:10:48 +0000

    Revert commenting the tests.

--- src/test/TestRunner.c
+++ src/test/TestRunner.c
@@ -129,13 +129,13 @@ main (int argc, char* argv[])
   //SRunner *runner = srunner_create( create_suite_Constraints() );
 
   srunner_add_suite( runner, create_suite_CVTerms() );
-  //srunner_add_suite( runner, create_suite_Basic() );
-  //srunner_add_suite( runner, create_suite_Errors() );
-  //srunner_add_suite( runner, create_suite_Hierarchy() );
-  //srunner_add_suite( runner, create_suite_Flattening() );
-  //srunner_add_suite( runner, create_suite_Distrib() );
-  //srunner_add_suite( runner, create_suite_FBC() );
-  //srunner_add_suite( runner, create_suite_Constraints() );
+  srunner_add_suite( runner, create_suite_Basic() );
+  srunner_add_suite( runner, create_suite_Errors() );
+  srunner_add_suite( runner, create_suite_Hierarchy() );
+  srunner_add_suite( runner, create_suite_Flattening() );
+  srunner_add_suite( runner, create_suite_Distrib() );
+  srunner_add_suite( runner, create_suite_FBC() );
+  srunner_add_suite( runner, create_suite_Constraints() );
 
 
 #ifdef TRACE_MEMORY
